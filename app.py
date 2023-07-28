7290d21144048f9e83165d5f668373889d232e48
praneshvr00 <vrpranesh555@gmail.com>
2023-07-28 11:05:23 +0530

    Added accounts.json to store

--- app.py
+++ app.py
@@ -44,10 +44,22 @@ loginError = {
 
 @app.route('/api/login/', methods=['POST'])
 def login():
-    data = request.get_json()
-    if(data['email'] == userdata['email'] and data['password'] == userdata['password']):
-        return loginData
+    responseData = request.get_json()
+    with open("accounts.json") as file:
+        userAccount = json.load(file)
+    if(userAccount != []):
+        for data in userAccount:
+            if(data['email'] == responseData['email'] and data['password'] == responseData['password']):
+                print(data['email'] + ',' + data['password'])
+                return loginData
     return loginError
+
+@app.route('/api/categories/', methods=['GET'])
+def categories():
+    with open("categories.json", "r") as file:
+        appData = json.load(file)
+        return appData
+
 
 @app.route('/api/register/', methods = ['POST'])
 def register():
@@ -74,6 +86,23 @@ def register():
             "code": 910,
             "message": "Email Exist"})
     
+# @app.route('/api/otp', methods=['GET'])
+# def otp():
+#     client = vonage.Client(key="05c55fc9", secret="JqWUbezjFL8Azp72")
+#     sms = vonage.Sms(client)
+#     otp = random.randint(1000, 9999)
+#     responseData = sms.send_message(
+#     {
+#         "from": "Vonage APIs",
+#         "to": "919342602060",
+#         "text": otp,
+#     }
+#     )
+
+#     if responseData["messages"][0]["status"] == "0":
+#         return "OTP sent"
+#     else:
+#        return(f"Message failed with error: {responseData['messages'][0]['error-text']}")
 
 if __name__ == '__main__':
     app.run(debug=False, host="0.0.0.0")
